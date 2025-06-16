651b3181595aade6d0bb5fae6e3b3d8b653f7a58
John <jjhhoward1738@gmail.com>
2025-06-16 14:18:07 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -1,9 +1,7 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,jsonify
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -11,9 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -22,6 +19,25 @@ def index():
 
 # Add views here
 
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+    if quake:
+        return jsonify(quake.to_dict()), 200
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+    
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_quakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    return jsonify({
+        "count": len(quakes),
+        "quakes": [q.to_dict() for q in quakes]
+    }), 200
+
